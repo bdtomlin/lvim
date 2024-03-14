22e62915304480df7cd043b4be6d40585c8ebe41
Bryan Tomlin <bdtomlin@gmail.com>
2024-03-14 11:50:28 -0500

    fix issue with nvim tree when opening lvim with a directory

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 lvim.plugins = {
 	{ "savq/melange-nvim" },
@@ -52,23 +54,27 @@ lvim.builtin.lualine.sections.lualine_c = {
 -- End Status Line Config
 
 -- NvimTree Config
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 lvim.builtin.nvimtree.setup.hijack_unnamed_buffer_when_opening = true
 lvim.builtin.nvimtree.setup.disable_netrw = true
 lvim.builtin.nvimtree.setup.hijack_netrw = true
-lvim.builtin.nvimtree.setup.view.float.open_win_config.relative = "win"
--- lvim.builtin.nvimtree.setup.hijack_directories.enable = true
+lvim.builtin.nvimtree.setup.hijack_directories.enable = true
+lvim.builtin.nvimtree.setup.view.float.enable = true
 lvim.builtin.nvimtree.setup.view.float.open_win_config.width = 40
 lvim.builtin.nvimtree.setup.view.float.open_win_config.height = 45
-lvim.builtin.nvimtree.setup.view.float.enable = true
+lvim.builtin.nvimtree.setup.view.float.open_win_config.relative = "win"
 lvim.builtin.nvimtree.setup.actions.open_file.window_picker.enable = false
-lvim.builtin.which_key.mappings.c = { "<cmd>nohlsearch<CR>", "No Highlight" }
-lvim.builtin.which_key.mappings.k = { "<cmd>BufferKill<CR>", "Kill Buffer" }
+
+-- the following 2 settings are needed to prevent errors when 
+-- using lvim . and then running telescope.
+lvim.builtin.nvimtree.setup.view.float.quit_on_focus_loss = false
+lvim.builtin.nvimtree.setup.actions.open_file.quit_on_open = true
 
 vim.cmd([[
 	command! -nargs=? -complete=dir Explore silent edit .
 	command! -nargs=? -complete=dir Reveal silent edit %:h
 ]])
 -- end NvimTree Config
+
+lvim.builtin.which_key.mappings.c = { "<cmd>nohlsearch<CR>", "No Highlight" }
+lvim.builtin.which_key.mappings.k = { "<cmd>BufferKill<CR>", "Kill Buffer" }
